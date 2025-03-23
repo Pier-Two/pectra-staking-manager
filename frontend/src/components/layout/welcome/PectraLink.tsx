1e70e42d5ede44ecaeea4459125cb17632498161
stuart-collinson <stuartcollinson4@hotmail.com>
2025-03-24 09:53:44 +1000

    Updating version of Next.js and fixing welcome screen UI for the Pectra announcement link

--- frontend/src/components/layout/welcome/PectraLink.tsx
+++ frontend/src/components/layout/welcome/PectraLink.tsx
@@ -3,12 +3,12 @@ import { ChevronRight } from "lucide-react";
 
 export const PectraLink: FC = () => {
   return (
-    <div className="p-[1px] rounded-xl bg-gradient-to-r from-[#00FFA7] via-[#5164DC] to-[#313C86] w-[20vw] text-xs overflow-auto">
+    <div className="w-[30%] p-[1px] rounded-xl bg-gradient-to-r from-[#00FFA7] via-[#5164DC] to-[#313C86] text-xs overflow-auto">
       <a
         href="https://ethereum.org/en/roadmap/pectra/"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center justify-between rounded-xl bg-white pb-1 pe-6 ps-6 pt-1 transition-colors hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
+        className="flex items-center min-h-8 justify-between rounded-xl bg-white pb-1 pe-6 ps-6 pt-1 transition-colors hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700"
       >
         <div>Pectra is here!</div>
         <div className="flex items-center gap-2">
