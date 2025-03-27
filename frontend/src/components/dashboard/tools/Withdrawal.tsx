d8d0036e759bdae41728540c4e47646120ea8519
stuart-collinson <stuartcollinson4@hotmail.com>
2025-03-27 16:59:15 +1000

    Changes to the tool widget to make the site feel more friendly

--- frontend/src/components/dashboard/tools/Withdrawal.tsx
+++ frontend/src/components/dashboard/tools/Withdrawal.tsx
@@ -14,22 +14,22 @@ export const Withdrawal: FC = () => {
 
   return (
     <div
-      className="hover:border-3 group flex flex-col space-y-4 rounded-xl border border-indigo-200 bg-white p-4 pe-8 ps-8 text-white hover:cursor-pointer dark:border-gray-700 dark:bg-black"
+      className="hover:bg-activeCard hover:border-3 group flex flex-col space-y-4 rounded-xl border border-indigo-200 bg-white p-4 pe-8 ps-8 text-gray-900 hover:cursor-pointer hover:text-white dark:border-gray-700 dark:bg-black dark:text-white"
       onClick={handleWithdrawalNavigation}
     >
-      <div className="flex flex-row items-center gap-x-2 text-indigo-800 dark:text-indigo-200">
+      <div className="flex flex-row items-center gap-x-2 text-indigo-800 group-hover:text-green-400 dark:text-indigo-200">
         <ArrowUpFromDot size={30} />
         <div className="text-2xl font-medium">Partial Withdrawal</div>
       </div>
 
       <div className="flex flex-col gap-y-4 pt-8">
-        <div className="text-gray-900 dark:text-white">
+        <div>
           Submit onchain execution layer withdrawal requests against validators,
           as per Pectra EIP-7002.
         </div>
 
         <PrimaryButton
-          className="w-[40%]"
+          className="w-auto group-hover:bg-white group-hover:text-indigo-800"
           label="Withdraw now"
           disabled={false}
           onClick={handleWithdrawalNavigation}
