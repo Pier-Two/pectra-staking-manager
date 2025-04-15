0951f71dee6a67855491a4e956320d3490d85648
Jordan Aston <jordanaston93@gmail.com>
2025-04-15 10:38:29 +1000

    style: jordan/glow-button-welcome-page

--- frontend/src/components/layout/welcome/PectraLink.tsx
+++ frontend/src/components/layout/welcome/PectraLink.tsx
@@ -1,19 +1,25 @@
 import { ChevronRight } from "lucide-react";
 import { Button } from "pec/components/ui/button";
+import { GlowEffect } from "pec/components/ui/glow-effect";
 import type { FC } from "react";
 
 export const PectraLink: FC = () => {
   return (
-    <div className="relative inline-block">
-      <div className="absolute inset-0 rounded-full bg-[linear-gradient(170.54deg,#00FFA7_11.34%,#5164DC_31.73%,#313C86_59.22%,rgba(113,255,224,0.8)_100%)]">
-        <div className="absolute inset-[1px] rounded-full bg-indigo-50 dark:bg-gray-950"></div>
-      </div>
+    <div className="relative">
+      <GlowEffect
+        colors={["#FDBA74", "#5164DC", "#86EFAC", "#313C86"]}
+        mode="pulse"
+        blur="softest"
+        duration={3}
+        scale={1}
+      />
+
       <Button
         onClick={() => {
           window.open("https://ethereum.org/en/roadmap/pectra/", "_blank");
         }}
         variant="outline"
-        className="relative !h-auto gap-x-4 rounded-full border-0 bg-[linear-gradient(170.54deg,rgba(253,186,116,0.15)_11.34%,rgba(165,180,252,0.15)_53.87%,rgba(134,239,172,0.15)_100%)] !px-4 !py-2"
+        className="relative !h-auto gap-x-4 rounded-full border-border bg-zinc-50 !px-4 !py-2 hover:bg-zinc-50 dark:border-gray-700 dark:bg-gray-950 dark:hover:bg-gray-950"
       >
         <p className="mt-[1px] text-[13px] font-570 leading-[13px] text-zinc-950 dark:text-zinc-50">
           Pectra is here!
