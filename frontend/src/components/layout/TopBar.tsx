14a1fc604edce60b3f555b3fea650cf9436b9a38
Jordan Aston <jordanaston93@gmail.com>
2025-04-22 17:11:43 +1000

    feat: PEC-174-implement-navigation-header

--- frontend/src/components/layout/TopBar.tsx
+++ frontend/src/components/layout/TopBar.tsx
@@ -1,27 +1,41 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { ConnectWalletButton } from "pec/components/ui/wallet/ConnectWallet";
+import { FC } from "react";
+import { cn } from "pec/lib/utils";
 
 import DarkMode from "../dark-mode";
 import { SidebarTrigger } from "../ui/sidebar";
+import { Tools } from "./Tools";
+import { useValidators } from "pec/hooks/useValidators";
 
 export interface ITopBar {
-  numberOfValidators: number;
-  type: "profile" | "wallet_connect";
+  type?: "profile" | "wallet_connect";
 }
 
-export const TopBar = () => {
+export const TopBar: FC<ITopBar> = (props) => {
+  const { type } = props;
   const router = useRouter();
+  const pathname = usePathname();
+  const { data: validators } = useValidators();
 
   const handleWelcomeNavigation = () => {
     router.push("/welcome");
   };
 
+  const handleDashboardNavigation = () => {
+    router.push("/dashboard");
+  };
+
+  const handleChartsNavigation = () => {
+    router.push("/charts");
+  };
+
   return (
     <header className="sticky top-0 z-50 flex w-full items-center justify-between border-b border-zinc-200 bg-white/40 p-4 shadow-sm backdrop-blur-md dark:border-gray-800 dark:bg-gray-950">
-      <div className="order-2 flex items-center md:order-1">
+      <div className="order-2 mr-4 flex items-center md:order-1 md:mr-0">
         <div
           className="flex flex-row-reverse items-center gap-x-3 hover:cursor-pointer md:flex-row"
           onClick={handleWelcomeNavigation}
@@ -34,17 +48,61 @@ export const TopBar = () => {
           />
 
           <div className="flex flex-col gap-y-[3px] text-right md:text-left">
-            <h1 className="font-570 text-[19.8px] leading-[18px] dark:text-white">
+            <h1 className="text-[19.8px] font-570 leading-[18px] dark:text-white">
               Pectra Staking
             </h1>
-            <h1 className="font-570 text-[19.8px] leading-[18px] dark:text-white">
+            <h1 className="text-[19.8px] font-570 leading-[18px] dark:text-white">
               Manager
             </h1>
           </div>
         </div>
       </div>
 
-      <div className="order-1 flex items-center gap-4 md:order-2">
+      {type === "profile" && (
+        <div className="order-3 hidden items-center gap-[clamp(1rem,2.5vw,4rem)] md:flex">
+          <div className="flex items-center space-x-2">
+            <div className="hidden font-inter text-black dark:text-zinc-50 lg:block">
+              My Validators
+            </div>
+
+            <div
+              className="relative flex h-6 w-6 items-center justify-center rounded-[3px] font-inter text-black dark:text-zinc-50"
+              style={{
+                background:
+                  "linear-gradient(130.54deg, #00FFA7 11.34%, #5164DC 31.73%, #313C86 59.22%, rgba(113, 255, 224, 0.8) 100%)",
+              }}
+            >
+              <div className="absolute inset-[1px] rounded-[3px] bg-white dark:bg-gray-950" />
+              <p className="relative text-sm font-medium">
+                {validators?.length}
+              </p>
+            </div>
+          </div>
+
+          <Tools />
+
+          <div
+            className={cn(
+              "font-inter transition-colors duration-200 hover:cursor-pointer hover:text-zinc-500 dark:text-zinc-50 dark:hover:text-zinc-400",
+              pathname === "/dashboard" && "text-zinc-500 dark:text-zinc-400",
+            )}
+            onClick={handleDashboardNavigation}
+          >
+            Dashboard
+          </div>
+          <div
+            className={cn(
+              "font-inter transition-colors duration-200 hover:cursor-pointer hover:text-zinc-500 dark:text-zinc-50 dark:hover:text-zinc-400",
+              pathname === "/charts" && "text-zinc-500 dark:text-zinc-400",
+            )}
+            onClick={handleChartsNavigation}
+          >
+            Charts
+          </div>
+        </div>
+      )}
+
+      <div className="order-1 mr-4 flex items-center gap-4 md:order-3">
         <div className="md:hidden">
           <SidebarTrigger />
         </div>
