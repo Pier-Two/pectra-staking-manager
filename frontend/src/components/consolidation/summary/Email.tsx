3159ee073d0f1ef7fcf6f6c2c2ee71df073005ea
Benjamin Davies <148213274+BenDavies1218@users.noreply.github.com>
2025-04-24 16:17:35 +1000

    style: :lipstick: pec-added-some-styling

--- frontend/src/components/consolidation/summary/Email.tsx
+++ frontend/src/components/consolidation/summary/Email.tsx
@@ -10,8 +10,8 @@ export const Email: FC<IConsolidationEmail> = (props) => {
   const { cardText, cardTitle, summaryEmail, setSummaryEmail, errors, showEmail, setShowEmail } = props;
 
   return (
-    <div className="flex w-full flex-col items-center justify-between gap-x-4 space-y-4 rounded-xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-black">
-      <div className="flex-col-2 flex w-full items-center justify-between gap-x-4">
+    <div className="flex w-full flex-col justify-between rounded-xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-black">
+      <div className="flex-col-2 flex w-full items-center justify-between py-1">
         <div className="flex items-center gap-x-4">
           <Mail className="h-5 w-5 fill-indigo-500 text-gray-200 dark:text-black" />
           <div className="flex-col items-center">
@@ -33,17 +33,19 @@ export const Email: FC<IConsolidationEmail> = (props) => {
       </div>
 
       {showEmail && (
-        <Input
-          className="w-full rounded-xl border border-indigo-200 bg-white p-4 dark:border-gray-800 dark:bg-black"
-          placeholder="Email"
-          value={summaryEmail}
-          onChange={(e) => setSummaryEmail(e.target.value)}
-        />
+        <div className="flex flex-col gap-y-2 pt-4">
+          <Input
+            className="w-full rounded-xl border border-indigo-200 bg-white p-4 dark:border-gray-800 dark:bg-black"
+            placeholder="Email"
+            value={summaryEmail}
+            onChange={(e) => setSummaryEmail(e.target.value)}
+          />
+
+          <div className="mt-1 text-xs text-red-500">
+            {errors?.email && "Invalid email address"}
+          </div>
+        </div>
       )}
-      
-      <div className="mt-1 text-xs text-red-500">
-        {errors?.email && "Invalid email address"}
-      </div>
     </div>
   );
 };
