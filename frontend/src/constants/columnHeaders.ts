e016a9f3d004a6ccf4d122274bb498b4920864a7
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-04-28 06:59:21 +1000

    fix: build

--- frontend/src/constants/columnHeaders.ts
+++ frontend/src/constants/columnHeaders.ts
@@ -1,3 +1,4 @@
+import { ValidatorDetails } from "pec/types/validator";
 import type {
   IHeaderConfig,
   TableValidatorDetails,
@@ -46,13 +47,14 @@ export const SIGN_WITHDRAWAL_COLUMN_HEADERS = [
   { label: "Status", showSort: false },
 ] as const;
 
-export const DASHBOARD_VALIDATOR_COLUMN_HEADERS: IHeaderConfig[] = [
-  { label: "Validator", sortKey: "validatorIndex", mobile: true },
-  { label: "Active since", sortKey: "activeSince" },
-  { label: "Credentials", sortKey: "withdrawalAddress", mobile: true },
-  { label: "Status", sortKey: "status" },
-  { label: "Balance", sortKey: "balance", mobile: true },
-];
+export const DASHBOARD_VALIDATOR_COLUMN_HEADERS: IHeaderConfig<ValidatorDetails>[] =
+  [
+    { label: "Validator", sortKey: "validatorIndex", mobile: true },
+    { label: "Active since", sortKey: "activeSince" },
+    { label: "Credentials", sortKey: "withdrawalAddress", mobile: true },
+    { label: "Status", sortKey: "status" },
+    { label: "Balance", sortKey: "balance", mobile: true },
+  ];
 
 export const SIGN_DEPOSIT_COLUMN_HEADERS = [
   {
--- frontend/src/constants/consolildation.ts
+++ frontend/src/constants/consolildation.ts
@@ -1,6 +1,7 @@
+import { ValidatorDetails } from "pec/types/validator";
 import { IHeaderConfig } from "pec/types/validatorTable";
 
-export const CONSOLIDATION_TABLE_HEADERS: IHeaderConfig[] = [
+export const CONSOLIDATION_TABLE_HEADERS: IHeaderConfig<ValidatorDetails>[] = [
   { label: "Validator", sortKey: "validatorIndex", mobile: true },
   { label: "Credentials", sortKey: "withdrawalAddress", mobile: true },
   { label: "Balance", sortKey: "balance", mobile: true },
