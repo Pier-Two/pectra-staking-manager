6aa7c9e327137579dc8b6a130953e1b6cd6b04b6
Benjamin Davies <148213274+BenDavies1218@users.noreply.github.com>
2025-04-29 14:22:18 +1000

    style: :lipstick: pec-fixed-the-logos

--- frontend/src/components/layout/BottomBar.tsx
+++ frontend/src/components/layout/BottomBar.tsx
@@ -2,52 +2,62 @@
 
 import Image from "next/image";
 import { logoPaths } from "pec/constants/logo";
+import { labrysUrl, openInNewTab, pierTwoUrl } from "pec/helpers/getExternalLink";
 import { useTheme } from "pec/hooks/useTheme";
+import { cn } from "pec/lib/utils";
 import { EThemeMode } from "pec/types/theme";
 import type { FC } from "react";
 
-export const BottomBar: FC = () => {
+interface RenderLogoProps {
+  grayscale?: boolean;
+  logo: "PierTwo" | "Labrys";
+  width: number;
+  height: number;
+}
+
+export const RenderLogo = ({ logo, grayscale = false, width, height }: RenderLogoProps) => {
   const { darkMode } = useTheme();
   const mode: EThemeMode = darkMode ? EThemeMode.DARK : EThemeMode.LIGHT;
+  const currentLogo = logoPaths[logo];
+  if (!currentLogo) return null;
 
-  const renderLogo = (logo: string) => {
-    const currentLogo = logoPaths[logo];
-    if (!currentLogo) return null;
+  const logoSrc = currentLogo[mode] ?? "";
 
-    const logoSrc = currentLogo[mode] ?? "";
+  return (
+    <div className="group relative">
+      <Image
+        src={logoSrc}
+        alt={logo}
+        className={cn({
+          "transition-grayscale grayscale duration-200 group-hover:grayscale-0": grayscale,
+        })}
+        width={width}
+        height={height}
+      />
+    </div>
+  );
+};
 
-    return (
-      <div className="group relative">
-        <Image
-          src={logoSrc}
-          alt={logo}
-          className="h-35 w-35 transition-grayscale grayscale duration-200 group-hover:grayscale-0"
-          width={100}
-          height={100}
-        />
-      </div>
-    );
-  };
-
+export const BottomBar: FC = () => {
   return (
     <footer className="fixed bottom-0 z-10 flex flex-col sm:flex-row p-4 w-full items-center justify-center sm:justify-between border-t gap-y-4 bg-[rgba(255,255,255,0.98)] px-6 shadow-sm dark:border-gray-800 dark:bg-gray-950">
       <div className="text-[11px] leading-[11px] text-zinc-950 dark:text-gray-300 text-center sm:text-left">
         Built with 🩶 by and for the Ethereum community
       </div>
 
-      <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-8 text-sm text-gray-600 dark:text-gray-300">
+      <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-8 text-sm text-gray-600 dark:text-gray-300 hover:cursor-pointer" onClick={() => openInNewTab(pierTwoUrl)}>
         <div className="flex flex-wrap items-center justify-center gap-x-3">
           <p className="text-[11px] leading-[11px] text-zinc-950 dark:text-gray-300">
             Product by:
           </p>
-          {renderLogo("PierTwo")}
+          <RenderLogo logo="PierTwo" grayscale={true} width={100} height={100} />
         </div>
 
-        <div className="flex flex-wrap items-center justify-center gap-x-3">
+        <div className="flex flex-wrap items-center justify-center gap-x-3 hover:cursor-pointer" onClick={() => openInNewTab(labrysUrl)}>
           <p className="text-[11px] leading-[11px] text-zinc-950 dark:text-gray-300">
             Produced by:{" "}
           </p>
-          {renderLogo("Labrys")}
+          <RenderLogo logo="Labrys" grayscale={true} width={100} height={100} />
         </div>
       </div>
     </footer>
--- frontend/src/components/layout/welcome/Footer.tsx
+++ frontend/src/components/layout/welcome/Footer.tsx
@@ -1,42 +1,29 @@
 "use client";
 
-import Image from "next/image";
-import { useTheme } from "pec/hooks/useTheme";
+import { labrysUrl, openInNewTab, pierTwoUrl } from "pec/helpers/getExternalLink";
 import type { FC } from "react";
+import { RenderLogo } from "../BottomBar";
 
 export const Footer: FC = () => {
-  const { darkMode } = useTheme();
 
   return (
     <div className="mt-9 flex flex-col items-center font-inter text-xs font-light">
       <div className="flex flex-row gap-20 text-gray-500 dark:text-gray-400">
-        <div className="flex flex-col items-center gap-y-3">
+        <div className="flex flex-col items-center gap-y-3 hover:cursor-pointer" onClick={() => openInNewTab(pierTwoUrl)}>
           <div className="text-[11px] leading-[11px] text-zinc-600">
             Product by
           </div>
-          <Image
-            src={`/logos/${darkMode ? "dark" : "light"}/PierTwo.svg`}
-            alt="Pier Two"
-            width={999}
-            height={999}
-            className="h-6 w-[124.24px]"
-          />
+          <RenderLogo logo="PierTwo" width={150} height={150} />
           <div className="text-[11px] leading-[11px] text-zinc-600">
             Onchain infrastructure
           </div>
         </div>
 
-        <div className="flex flex-col items-center gap-y-3">
+        <div className="flex flex-col items-center gap-y-3 hover:cursor-pointer" onClick={() => openInNewTab(labrysUrl)}>
           <div className="text-[11px] leading-[11px] text-zinc-600">
             Produced by
           </div>
-          <Image
-            src={`/logos/${darkMode ? "dark" : "light"}/Labrys.svg`}
-            alt="Labrys"
-            width={999}
-            height={999}
-            className="h-6 w-[124.24px]"
-          />
+          <RenderLogo logo="Labrys" width={150} height={150} />
           <div className="text-[11px] leading-[11px] text-zinc-600">
             Onchain developers
           </div>
