a34a4c31f4ac23ab598d370604bdbc3dea369500
Aidan Kinzett <aidan@kinzett.io>
2025-04-30 13:17:35 +1000

    fix: fix layout issue, use redis cache

--- frontend/src/components/dashboard/validators/TotalDailyIncome.tsx
+++ frontend/src/components/dashboard/validators/TotalDailyIncome.tsx
@@ -6,29 +6,43 @@ import { formatUnits } from "viem";
 import { displayedEthAmount } from "pec/lib/utils/validators/balance";
 import { Skeleton } from "pec/components/ui/skeleton";
 import { useValidatorPerformance } from "pec/hooks/useValidatorPerformance";
-import { useEthPrice } from "pec/hooks/useEthPrice";
 import { dashboardAnimationDelays } from "pec/constants/animationDelays";
+import { api } from "pec/trpc/react";
 
 export const TotalDailyIncome = () => {
-  const { data: ethPrice, isSuccess: isEthPriceSuccessful } = useEthPrice(
-    "ETH",
-    "USD",
-  );
+  const { data: ethPrice, isSuccess: isEthPriceSuccessful } =
+    api.pricing.getCurrentEthPrice.useQuery({
+      symbol: "ETH",
+      convert: "USD",
+    });
 
   const {
     data: validatorPerformanceInWei,
     isSuccess: isPerformanceSuccessful,
   } = useValidatorPerformance("daily");
 
-  if (
-    !(
-      isEthPriceSuccessful &&
-      isPerformanceSuccessful &&
-      ethPrice &&
-      validatorPerformanceInWei
-    )
-  )
-    return <TotalDailyIncomeLoading />;
+  if (!isEthPriceSuccessful || !isPerformanceSuccessful)
+    return (
+      <MyValidatorsCard
+        isLoading
+        layoutId="total-daily-income-card"
+        delay={dashboardAnimationDelays.totalDailyIncome}
+        title="Total Daily Income"
+        body={
+          <div className="flex flex-row items-center gap-x-2">
+            <span>Ξ</span>
+            <Skeleton className="h-8 w-16 bg-slate-50" />
+          </div>
+        }
+        subtext={
+          <div className="flex flex-row items-center gap-x-2">
+            <span>Earning</span>
+            <Skeleton className="mt-1 h-3 w-12 bg-slate-50" />
+            <span>per day</span>
+          </div>
+        }
+      />
+    );
 
   const totalInEth = parseFloat(
     formatUnits(BigInt(validatorPerformanceInWei), 18),
@@ -46,25 +60,3 @@ export const TotalDailyIncome = () => {
     />
   );
 };
-
-export const TotalDailyIncomeLoading = () => (
-  <MyValidatorsCard
-    isLoading
-    layoutId="total-daily-income-card"
-    delay={dashboardAnimationDelays.totalDailyIncome}
-    title="Total Daily Income"
-    body={
-      <div className="flex flex-row items-center gap-x-2">
-        <span>Ξ</span>
-        <Skeleton className="h-8 w-16 bg-slate-50" />
-      </div>
-    }
-    subtext={
-      <div className="flex flex-row items-center gap-x-2">
-        <span>Earning</span>
-        <Skeleton className="mt-1 h-3 w-12 bg-slate-50" />
-        <span>per day</span>
-      </div>
-    }
-  />
-);
