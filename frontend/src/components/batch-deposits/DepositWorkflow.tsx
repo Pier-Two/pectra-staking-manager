698d6863b9a7d5f2438e465e773bb4b9576a2af2
Jordan Aston <jordanaston93@gmail.com>
2025-05-01 09:06:39 +1000

    fix: remove duplicate batch deposit header

--- frontend/src/components/batch-deposits/DepositWorkflow.tsx
+++ frontend/src/components/batch-deposits/DepositWorkflow.tsx
@@ -176,17 +176,6 @@ export const DepositWorkflow = ({
   return (
     <div className="flex w-full flex-col gap-y-4">
       <div className="space-y-8">
-        <div className="flex flex-col gap-4">
-          <div className="flex gap-x-4 text-primary-dark dark:text-indigo-300">
-            <ArrowDownToDot className="h-8 w-8 self-center" />
-            <div className="text-3xl font-medium">Batch Deposit</div>
-          </div>
-
-          <div className="text-base">
-            Top up your existing validators in one transaction.
-          </div>
-        </div>
-
         {stage.type === "data-capture" && (
           <>
             {balance === 0 ? (
