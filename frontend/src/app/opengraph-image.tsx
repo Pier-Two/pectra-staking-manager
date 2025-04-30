d1bb544f8af8e478850e4978c48bb1d80334a22c
Aidan Kinzett <aidan@kinzett.io>
2025-04-30 16:30:41 +1000

    fix: replace with static image

--- frontend/src/app/opengraph-image.tsx
+++
@@ -1,7 +0,0 @@
-import { generateOpenGraphImage } from "pec/components/opengraph-image";
-
-export default async function OpenGraphImage() {
-  return generateOpenGraphImage();
-}
-
-export { size, contentType } from "pec/components/opengraph-image";
