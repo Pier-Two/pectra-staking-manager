720e6ece59bb86c30c14a7299a2f654eacc93436
Aidan Kinzett <aidan@kinzett.io>
2025-05-01 10:40:50 +1000

    revert: revert wording in og metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -26,9 +26,9 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Pectra Staking Manager",
+  title: "Pectra Staking Manager: This is the Future of Ethereum Staking",
   description:
-    "A self-service tool designed solely to help you consolidate your Ethereum validators to the new Pectra standard in a few clicks, enabling higher balances, auto-compounding rewards and fast onchain withdrawals.",
+    "Take control of your validators with the Pectra upgrade! Upgrade to Pextra (0x02), and consolidate multiple validators for easier management, perform batch top-ups to the new max value from 32ETH to 2048ETH, perform withdraws, and monitor the adoption of Pectra via our charts.",
   icons: [{ rel: "icon", url: "/favicon.svg" }],
 };
 
