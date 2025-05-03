6ce0bab268bda626141e29d39cef788f1cf4668f
Aidan Kinzett <aidan@kinzett.io>
2025-05-03 18:21:05 +1000

    feat: text change for unstake page

--- frontend/src/app/(narrow-layout)/withdraw/page.tsx
+++ frontend/src/app/(narrow-layout)/withdraw/page.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 import { title } from "pec/constants/metadata";
 
 export const metadata: Metadata = {
-  title: title("Withdraw"),
+  title: title("Unstake"),
 };
 
 const WithdrawPage = () => {
@@ -14,12 +14,17 @@ const WithdrawPage = () => {
         <div className="flex flex-col gap-4">
           <div className="flex gap-x-4 text-primary-dark dark:text-indigo-300">
             <ArrowUpFromDot className="h-8 w-8 self-center" />
-            <div className="text-3xl font-medium">Withdrawal</div>
+            <div className="text-3xl font-medium">Unstake</div>
           </div>
 
           <div className="text-base">
-            Submit onchain execution layer withdrawal requests against
-            validators, as per Pectra EIP-7002.
+            Unstake from your validators using your web3 wallet. You can use
+            this tool to perform partial withdrawals and full exits.
+          </div>
+          <div className="text-base">
+            One benefit of partial withdrawals is how much faster the ETH will
+            land in your account, for example, if the queue is empty, it will
+            take 27 hours for 2000 ETH to land back into your account.
           </div>
         </div>
         <Withdrawal />
