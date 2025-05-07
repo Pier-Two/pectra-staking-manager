7ac669d35f2df53df33d723d91f08db8219b200b
Aidan Kinzett <aidan@kinzett.io>
2025-05-07 11:32:59 +1000

    feat: move root redirection to the client

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,25 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { PectraSpinner } from "pec/components/ui/custom/pectraSpinner";
+import { useEffect } from "react";
+import { useActiveWalletConnectionStatus } from "thirdweb/react";
 
 export default function Home() {
-  redirect("/welcome");
+  const connectionStatus = useActiveWalletConnectionStatus();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (connectionStatus === "connected") {
+      router.push("/dashboard");
+    } else {
+      router.push("/welcome");
+    }
+  }, [connectionStatus, router]);
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <PectraSpinner className="size-12" />
+    </div>
+  );
 }
