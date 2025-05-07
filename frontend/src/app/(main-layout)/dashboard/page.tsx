3dc8f8d5d91e744a11d3eefbc3dbd47d74738d74
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-05-07 11:44:47 +1000

    feat: added tooltips and banner

--- frontend/src/app/(main-layout)/dashboard/page.tsx
+++ frontend/src/app/(main-layout)/dashboard/page.tsx
@@ -6,6 +6,7 @@ import { DashboardValidatorTable } from "pec/components/dashboard/validatorTable
 import type { Metadata } from "next";
 import { title } from "pec/constants/metadata";
 import { RedirectWhenDisconnected } from "pec/hooks/use-redirect-when-disconnected";
+import { TriangleAlert } from "lucide-react";
 
 export const metadata: Metadata = {
   title: title("Dashboard"),
@@ -15,7 +16,22 @@ const Dashboard = () => (
   <>
     <RedirectWhenDisconnected />
     <div className="flex w-full flex-col items-center">
-      <div className="relative flex max-w-[80rem] items-center justify-center pb-12">
+      <div className="relative -mt-8 flex h-20 w-screen items-center bg-orange-100 dark:bg-orange-900">
+        <div className="flex max-w-[80rem] flex-col gap-1 px-2 py-8 md:px-8 xl:px-2">
+          <div className="flex gap-2">
+            <TriangleAlert fill="orange" />
+            <div className="text-base">
+              Notice: Validator Data May Be Inaccurate
+            </div>
+          </div>
+          <div className="text-sm">
+            Due to the ongoing Ethereum Pectra upgrade and updates being applied
+            to third-party services like beaconcha.in, some validator
+            information may not display correctly or be temporarily out of sync.
+          </div>
+        </div>
+      </div>
+      <div className="relative flex max-w-[80rem] items-center justify-center pb-12 pt-4">
         <div className="space-y-6">
           <div className="text-2xl font-medium text-indigo-800 dark:text-indigo-200">
             Tools
