f0fba226d6a65d2f0eb9ba1cbec25e51990f09a1
max-taylor <39660195+max-taylor@users.noreply.github.com>
2025-05-07 14:06:32 +1000

    fix: rpc client

--- frontend/src/hooks/useRpcClient.ts
+++ frontend/src/hooks/useRpcClient.ts
@@ -2,6 +2,7 @@ import { HOODI_CHAIN_DETAILS } from "pec/constants/chain";
 import { client } from "pec/lib/wallet/client";
 import { getRpcClient } from "thirdweb";
 import { useActiveChainWithDefault } from "./useChain";
+import { mainnet } from "thirdweb/chains";
 
 export const useRpcClient = () => {
   const chain = useActiveChainWithDefault();
@@ -11,6 +12,11 @@ export const useRpcClient = () => {
   if (chain.id === HOODI_CHAIN_DETAILS.id) {
     return getRpcClient({
       chain: HOODI_CHAIN_DETAILS,
+      client,
+    });
+  } else if (chain.id === mainnet.id) {
+    return getRpcClient({
+      chain: mainnet,
       client,
     });
   }
