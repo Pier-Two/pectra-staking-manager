f6588240f50940a60c8858d089db2a998d3538e9
Aidan Kinzett <aidan@kinzett.io>
2025-05-07 17:07:38 +1000

    fix: fix the dark mode icon

--- frontend/src/components/dark-mode.tsx
+++ frontend/src/components/dark-mode.tsx
@@ -14,17 +14,15 @@ const DarkMode = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <ClingableElement className="rounded-full">
-          <Button
-            variant="outline"
-            size="icon"
-            className="rounded-full border border-primary/30 bg-transparent hover:bg-primary/10 dark:border-gray-700 dark:bg-gray-950 dark:hover:bg-gray-900"
-          >
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </ClingableElement>
+        <Button
+          variant="outline"
+          size="icon"
+          className="rounded-full border border-primary/30 bg-transparent hover:bg-primary/10 dark:border-gray-700 dark:bg-gray-950 dark:hover:bg-gray-900"
+        >
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
